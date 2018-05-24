47c7963f7a0bea74dbf3897640e55ca46bb57f97
rrokkkam <rohith.rokkam@stonybrook.edu>
2018-05-23 18:48:17 -0600

    Replace subparsers with mutually exclusive args

--- sniffles.py
+++ sniffles.py
@@ -14,8 +14,8 @@ class Sniffer:
     def sniff(self, mode='filter', time=0, **kwargs):
         modes = {
             'hexdump': hexdump.hexdump,
+            'outfile': pcap.print_packet,
             'filter': _print_plaintext,
-            'outfile': pcap.print_packet
         }
 
         # need to print header of pcap if outfile
@@ -28,7 +28,7 @@ class Sniffer:
                 while True:
                     data, _ = self.socket.recvfrom(_snaplen)
                     modes[mode](data, kwargs)
-        except TimeoutError:  # , KeyboardInterrupt:
+        except (TimeoutError, KeyboardInterrupt):
             if mode == 'outfile':  # need to close it
                 kwargs['outfile'].close()
 
@@ -56,35 +56,35 @@ class Sniffer:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='sniffles', description='''Sniff packets.
-                        Use on a Linux machine with construct 2.9.39.''')
+    parser = argparse.ArgumentParser(prog='sniffles', description='''Sniff 
+                        packets received over Ethernet. Use on a Linux 
+                        machine with construct version at least 2.9.39.''')
+
     parser.add_argument('interface', metavar='INTERFACE',
                         help='Interface to listen for traffic on.')
     parser.add_argument('-t', '--timeout', type=int, default=0,
                         help='''Time to capture for (in seconds). If set to 0 or
                         unspecified, ^C must be sent to close the program.''')
 
-    format_parsers = parser.add_subparsers(dest='format')
-
-    hexdump_parser = format_parsers.add_parser('hexdump')
-    hexdump_parser.add_argument('-x', '--hexdump', action='store_true',
+    format_parser = parser.add_mutually_exclusive_group()
+    format_parser.add_argument('-x', '--hexdump', action='store_true',
                                 help='Write hexdump to stdout.')
-
-    outfile_parser = format_parsers.add_parser('outfile')
-    outfile_parser.add_argument('-o', '--outfile', metavar='OUTFILE',
+    format_parser.add_argument('-o', '--outfile', metavar='OUTFILE',
                                 help='Write pcapng to a file.')
-
-    filter_parser = format_parsers.add_parser('filter')
-    filter_parser.add_argument('-f', '--filter', nargs='+', 
+    format_parser.add_argument('-f', '--filter', nargs='+', 
                                choices=list(parse.HEADERS),
+                               default=list(parse.HEADERS),
                                help='''Write human-readable output for the 
                                specified protocol(s) to stdout.''')
 
     kwargs = vars(parser.parse_args())
-    print(kwargs)
-    sniffer = sniffles.Sniffer(kwargs.pop['interface'])
-    mode = kwargs.pop['format']
-    time = kwargs.pop['timeout']
-
-    exit()
-    sniffer.sniff(mode, time, **kwargs)
+    sniffer = Sniffer(kwargs.pop('interface'))
+    timeout = kwargs.pop('timeout')
+    
+    # TODO: find a cleaner way of determining which of the mutually exclusive args was passed.
+    if kwargs.pop('hexdump'):
+        sniffer.sniff('hexdump', timeout, **kwargs)
+    elif kwargs['outfile'] is None:  # filter
+        sniffer.sniff('filter', timeout, **kwargs)
+    else:
+        sniffer.sniff('outfile', timeout, **kwargs)        
